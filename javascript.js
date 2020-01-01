38446f54fc3e669505602f8534513474bf614c6f
João Victor <47643696+joaovdev@users.noreply.github.com>
2020-01-01 02:03:50 -0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,27 +1,11 @@
 let c = document.getElementById("myCanvas");
 let ctx = c.getContext("2d");
-let canvas = document.getElementById("myCanvas");
 let isMouseDown = false;
-let x, y;
-document.getElementById("myCanvas").onmousedown = function() {
-  isMouseDown = true;
-};
-document.getElementById("myCanvas").onmouseup = function() {
-  isMouseDown = false;
-};
-document.getElementById("myCanvas").onmousemove = function(event) {
-  x = event.clientX;
-  y = event.clientY;
-};
-pmouseX = 0;
-pmouseY = 0;
+let x = 0, y = 0;
+let previousX, previousY;
 
-let backgroundColour = "white";
-let nodeColour = "green";
-let edgeColour = "blue";
-let nodeSize = 8;
 
-let createCuboid = function(x, y, z, w, h, d) {
+let createCuboid = function (x, y, z, w, h, d) {
   let nodes = [
     [x, y, z],
     [x, y, z + d],
@@ -49,87 +33,84 @@ let createCuboid = function(x, y, z, w, h, d) {
   return { nodes: nodes, edges: edges };
 };
 
-let object = createCuboid(-250, -250, 50, 100, 100, 30);
-let nodes = object.nodes;
-let edges = object.edges;
+let cuboid = createCuboid(-120, -20, -20, 240, 40, 40);
 
-// Rotate shape around the z-axis
-let rotateZ3D = function(theta) {
-  let sinTheta = Math.sin(theta);
-  let cosTheta = Math.cos(theta);
+var rotateX3D = function (theta) {
+  var sinTheta = Math.sin(theta/10);
+  var cosTheta = Math.cos(theta/10);
+  // ctx.fillText("sin=" + Math.sin(theta), 60, 60);
 
-  for (let n = 0; n < nodes.length; n++) {
-    let node = nodes[n];
-    let x = node[0];
-    let y = node[1];
-    node[0] = x * cosTheta - y * sinTheta;
-    node[1] = y * cosTheta + x * sinTheta;
+  for (var n = 0; n < cuboid.nodes.length; n++) {
+    var node = cuboid.nodes[n];
+    var y = node[1];
+    var z = node[2];
+    node[1] = (y * cosTheta - z * sinTheta);
+    node[2] = (z * cosTheta + y * sinTheta);
   }
 };
 
-let rotateY3D = function(theta) {
-  let sinTheta = Math.sin(theta);
-  let cosTheta = Math.sin(theta);
+var rotateY3D = function (theta) {
+  var sinTheta = Math.sin(theta/10);
+  var cosTheta = Math.cos(theta/10);
 
-  for (let n = 0; n < nodes.length; n++) {
-    let node = nodes[n];
-    let x = node[0];
-    let z = node[2];
-    node[0] = x * cosTheta - z * sinTheta;
-    node[2] = z * cosTheta + x * sinTheta;
+  for (var n = 0; n < cuboid.nodes.length; n++) {
+    var node = cuboid.nodes[n];
+    var x = node[0];
+    var z = node[2];
+    node[0] = (x * cosTheta + z * sinTheta);
+    node[2] = (z * cosTheta - x * sinTheta);
   }
 };
 
-let rotateX3D = function(theta) {
-  let sinTheta = Math.sin(theta);
-  let cosTheta = Math.cos(theta);
 
-  for (let n = 0; n < nodes.length; n++) {
-    let node = nodes[n];
-    let y = node[1];
-    let z = node[2];
-    node[1] = y * cosTheta - z * sinTheta;
-    node[2] = z * cosTheta + y * sinTheta;
-  }
+// let fillBlank = function () {
+//   ctx.fillStyle = "white";
+//   ctx.fillRect(0, 0, c.width, c.height);
+//   ctx.fillStyle = "black";
+// };
+
+c.onmousedown = function () {
+  isMouseDown = true;
 };
+c.onmouseup = function () {
+  isMouseDown = false;
+};
+c.onmousemove = function (event) {
+  if (isMouseDown) {
+    previousX = x;
+    previousY = y;
+    x = event.clientX;
+    y = event.clientY;
+    rotateY3D(x - previousX);
+    rotateX3D(y - previousY);
+  }
+}
 
-rotateZ3D(30);
-rotateY3D(30);
-rotateX3D(30);
 
 setInterval(() => {
-  ctx.fillStyle = backgroundColour;
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  if (isMouseDown) {
 
-  // Draw edges
-  ctx.strokeStyle = edgeColour;
-  for (let e = 0; e < edges.length; e++) {
-    let n0 = edges[e][0];
-    let n1 = edges[e][1];
-    let node0 = nodes[n0];
-    let node1 = nodes[n1];
+  ctx.clearRect(0, 0, c.width, c.height);
+  for (var e = 0; e < cuboid.edges.length; e++) {
+    ctx.save();
+    ctx.translate(c.height, c.width/4);
+    var n0 = cuboid.edges[e][0];
+    var n1 = cuboid.edges[e][1];
+    var node0 = cuboid.nodes[n0];
+    var node1 = cuboid.nodes[n1];
     ctx.beginPath();
     ctx.moveTo(node0[0], node0[1]);
     ctx.lineTo(node1[0], node1[1]);
     ctx.stroke();
-  }
+    ctx.fillText("x=" + cuboid.nodes[0][0], 400, 50);
+    ctx.fillText("y=" + cuboid.nodes[0][1], 400, 60);
 
-  // Draw nodes
-  ctx.fillStyle = nodeColour;
-  for (let n = 0; n < nodes.length; n++) {
-    let node = nodes[n];
-    ctx.beginPath();
-    ctx.arc(node[0], node[1], nodeSize, 0, 2 * Math.PI);
-    ctx.fill();
-  }
-  if (isMouseDown) {
-    mouseDragged();
-  }
-}, 100);
+      ctx.fillText("x=" + x, x, y - 10);
+      ctx.fillText("y=" + y, x, y);
+      ctx.fillText("pX=" + (previousX - x), 400, 10);
+      ctx.restore();
 
-function mouseDragged() {
-  rotateY3D(x - pmouseX);
-  rotateX3D(y - pmouseY);
-  pmouseX = x;
-  pmouseY = y;
-}
+    }
+  }
+}, 1);
+
