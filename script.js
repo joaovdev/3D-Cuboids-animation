8b50c6015ed9b03a1cc93bd6574035c27d7dc3a9
João Victor <47643696+joaovdev@users.noreply.github.com>
2020-01-09 12:32:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -69,18 +69,20 @@ c.onmousedown = function () {
 c.onmouseup = function () {
     isMouseDown = false;
 };
+
+ctx.fillText("Drag here to rotate!", 20, 70);
 for (var e = 0; e < cuboid.edges.length; e++) { // drawing for the first time
-                ctx.save();
-                ctx.translate(c.width / 2, c.height / 2);
-                var n0 = cuboid.edges[e][0];
-                var n1 = cuboid.edges[e][1];
-                var node0 = cuboid.nodes[n0];
-                var node1 = cuboid.nodes[n1];
-                ctx.beginPath();
-                ctx.moveTo(node0[0], node0[1]);
-                ctx.lineTo(node1[0], node1[1]);
-                ctx.stroke();
-                ctx.restore();
+    ctx.save();
+    ctx.translate(c.width / 2, c.height / 2);
+    var n0 = cuboid.edges[e][0];
+    var n1 = cuboid.edges[e][1];
+    var node0 = cuboid.nodes[n0];
+    var node1 = cuboid.nodes[n1];
+    ctx.beginPath();
+    ctx.moveTo(node0[0], node0[1]);
+    ctx.lineTo(node1[0], node1[1]);
+    ctx.stroke();
+    ctx.restore();
 }
 
 
