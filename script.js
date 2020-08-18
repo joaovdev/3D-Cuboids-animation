65978780979edebc942ffc9db81e9ac751fbc0de
João Victor <47643696+joaovdev@users.noreply.github.com>
2020-08-18 17:54:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -34,19 +34,26 @@ let createCuboid = function (x, y, z, w, h, d) {
     return { nodes: nodes, edges: edges };
 };
 
-let cuboid = createCuboid(-120, -20, -20, 240, 40, 40);
+let shapes = [];
 
+for (let m = 0; m < 3; m++) {
+    for (let n = 0; n < 3; n++) {
+        shapes.push(createCuboid(-120, ((40 * n) - 60), ((40 * m) - 60), 240, 40, 40));
+    }
+}
 
 let rotateX3D = function (theta) {
     let sinTheta = Math.sin(theta / 50);
     let cosTheta = Math.cos(theta / 50);
 
-    for (let n = 0; n < cuboid.nodes.length; n++) {
-        let node = cuboid.nodes[n];
-        let y = node[1];
-        let z = node[2];
-        node[1] = (y * cosTheta - z * sinTheta);
-        node[2] = (z * cosTheta + y * sinTheta);
+    for (let q = 0; q < shapes.length; q++) {
+        for (let n = 0; n < shapes[q].nodes.length; n++) {
+            let node = shapes[q].nodes[n];
+            let y = node[1];
+            let z = node[2];
+            node[1] = (y * cosTheta - z * sinTheta);
+            node[2] = (z * cosTheta + y * sinTheta);
+        }
     }
 };
 
@@ -54,12 +61,14 @@ let rotateY3D = function (theta) {
     let sinTheta = Math.sin(theta / 50);
     let cosTheta = Math.cos(theta / 50);
 
-    for (let n = 0; n < cuboid.nodes.length; n++) {
-        let node = cuboid.nodes[n];
-        let x = node[0];
-        let z = node[2];
-        node[0] = (x * cosTheta + z * sinTheta);
-        node[2] = (z * cosTheta - x * sinTheta);
+    for (let q = 0; q < shapes.length; q++) {
+        for (let n = 0; n < shapes[q].nodes.length; n++) {
+            let node = shapes[q].nodes[n];
+            let x = node[0];
+            let z = node[2];
+            node[0] = (x * cosTheta + z * sinTheta);
+            node[2] = (z * cosTheta - x * sinTheta);
+        }
     }
 };
 
@@ -88,31 +97,34 @@ c.onmousemove = function (event) {
 
 setInterval(function () {
     ctx.clearRect(0, 0, c.width, c.height); // clears the canvas
-    let edgeXlength = cuboid.nodes[0][0] - cuboid.nodes[4][0];
-    let edgeYlength = cuboid.nodes[0][1] - cuboid.nodes[4][1];
-    let edgeSin = edgeYlength / 240;
-    let edgeCos = edgeXlength / 240;
-    let Wavy = Math.sin(FrameCounter / 20);
-    for (let e = 0; e < cuboid.edges.length; e++) { // draws cuboid
-        ctx.save(); // saves the ctx's initial position
-        ctx.translate(c.width / 2, c.height / 2); // moves ctx to the middle of canvas
-        let n0 = cuboid.edges[e][0]; // the array position of one of the edge's node
-        let n1 = cuboid.edges[e][1]; // the array position of the edge's other node
-        let node0 = cuboid.nodes[n0]; // one of node's coordinate(x,y,z)
-        let node1 = cuboid.nodes[n1]; // the other node's coordinate(x,y,z)
-        ctx.beginPath();
-        if (n0 < 5 && n1 < 4) {
-            ctx.moveTo(node0[0] + Wavy * edgeCos * 10, node0[1] + Wavy * edgeSin * 10);
-            ctx.lineTo(node1[0] + Wavy * edgeCos * 10, node1[1] + Wavy * edgeSin * 10);
-        } else if (n1 - n0 == 4) {
-            ctx.moveTo(node0[0] + Wavy * edgeCos * 10, node0[1] + Wavy * edgeSin * 10);
-            ctx.lineTo(node1[0], node1[1]);
-        } else {
-            ctx.moveTo(node0[0], node0[1]);
-            ctx.lineTo(node1[0], node1[1]);
-        }
+    let edgeLengthX = shapes[0].nodes[0][0] - shapes[0].nodes[4][0];
+    let edgeLengthY = shapes[0].nodes[0][1] - shapes[0].nodes[4][1];//getting the length of an edge 
+    let edgeSin = edgeLengthY / 240; // sin=opposite/hypotenuse
+    let edgeCos = edgeLengthX / 240; // cos=adjacent/hypotenuse
+    for (let q = 0; q < shapes.length; q++) {
+        let offset = 5 * q;
+        let Wavy = Math.sin((FrameCounter+offset) / 20);
+        for (let e = 0; e < shapes[q].edges.length; e++) { // draws cuboid
+            ctx.save(); // saves the ctx's initial position
+            ctx.translate(c.width / 2, c.height / 2); // moves ctx to the middle of canvas
+            let n0 = shapes[q].edges[e][0]; // the array position of one of the edge's node
+            let n1 = shapes[q].edges[e][1]; // the array position of the edge's other node
+            let node0 = shapes[q].nodes[n0]; // one of node's coordinate(x,y,z)
+            let node1 = shapes[q].nodes[n1]; // the other node's coordinate(x,y,z)
+            ctx.beginPath();
+            if (n0 < 5 && n1 < 4) {
+                ctx.moveTo(node0[0] + Wavy * edgeCos * 10, node0[1] + Wavy * edgeSin * 10);
+                ctx.lineTo(node1[0] + Wavy * edgeCos * 10, node1[1] + Wavy * edgeSin * 10);
+            } else if (n1 - n0 == 4) {
+                ctx.moveTo(node0[0] + Wavy * edgeCos * 10, node0[1] + Wavy * edgeSin * 10);
+                ctx.lineTo(node1[0], node1[1]);
+            } else {
+                ctx.moveTo(node0[0], node0[1]);
+                ctx.lineTo(node1[0], node1[1]);
+            }
             ctx.stroke();
             ctx.restore(); // restores ctx to it's initial position
         }
-        FrameCounter++;
-    }, 16.67); // 60x/second
+    }
+    FrameCounter++;
+}, 16.67); // 60x per second
